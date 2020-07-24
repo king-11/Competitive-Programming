bae7c798f03033f47d8c6faee6a4266139c988c0
king-11 <lakshay.singh1108@gmail.com>
2020-07-24 23:44:18 +0530

    base strcuture change

--- fastio.cpp
+++ fastio.cpp
@@ -23,22 +23,24 @@
 #define pii pair<int, int>
 #define minq priority_queue<int, vector<int>, greater<int>>
 #define mem(a, val) memset(a, val, sizeof(a))
-#define X 1000000009
-#define le(x) scanf("%lld", &x)
-#define lle(x, y) scanf("%lld %lld", &x, &y)
+#define scani(x) scanf("%lld", &x)
+#define scani2(x, y) scanf("%lld %lld", &x, &y)
+#define scani3(x, y, z) scanf("%lld %lld %lld", &x, &y, &z);
 #define print(x) printf("%lld ", x)
 #define println(x) printf("%lld\n", x)
 #define SpeedForce ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000009
 
 using namespace std;
 
+const int N = 3e5 + 11;
+
 int32_t main()
 {
-#ifndef ONLINE_JUDGE
-     freopen("input.txt", "r", stdin);
-     freopen("output.txt", "w", stdout);
-#endif
+     // #ifndef ONLINE_JUDGE
+     //      freopen("input.txt", "r", stdin);
+     //      freopen("output.txt", "w", stdout);
+     // #endif
 
      SpeedForce;
-     
 }
