de85cd2b30009db6e3750a22b860db18a988e25e
king-11 <lakshay.singh1108@gmail.com>
2020-07-24 23:43:53 +0530

    a2oj ladder B questions

---
+++ Codeforces - Codeforces Round #261 (Div. 2)/B. Pashmak and Flowers/fastio.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repA(i, a, n) for (int i = a; i <= (n); ++i)
+#define repD(i, a, n) for (int i = a; i >= (n); --i)
+#define tr(a, x) for (auto &a : x)
+#define all(c) c.begin(), c.end()
+
+#define lld long long
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define f first
+#define s second
+#define sz(a) int((a).size())
+#define present(c, x) (c.find(x) != c.end())
+#define cpresent(c, x) (find(all(c), x) != c.end())
+
+#define vi vector<lld>
+#define si set<lld>
+#define endl "\n"
+#define int long long
+#define pii pair<int, int>
+#define minq priority_queue<int, vector<int>, greater<int>>
+#define mem(a, val) memset(a, val, sizeof(a))
+#define scani(x) scanf("%lld", &x)
+#define scani2(x, y) scanf("%lld %lld", &x, &y)
+#define scani3(x, y, z) scanf("%lld %lld %lld", &x, &y, &z);
+#define print(x) printf("%lld ", x)
+#define println(x) printf("%lld\n", x)
+#define SpeedForce ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000009
+
+using namespace std;
+
+const int N = 2e5 + 11;
+
+int32_t main()
+{
+     // #ifndef ONLINE_JUDGE
+     //      freopen("input.txt", "r", stdin);
+     //      freopen("output.txt", "w", stdout);
+     // #endif
+
+     SpeedForce;
+     int n;
+     scani(n);
+     int mini = INT_MAX, maxi = INT_MIN;
+     int arr[n];
+     rep(i, n)
+     {
+          scani(arr[i]);
+          mini = min(*(arr + i), mini);
+          maxi = maxi > arr[i] ? maxi : arr[i];
+     }
+
+     int minc = 0, maxc = 0;
+     rep(i, n)
+     {
+          if (arr[i] == mini)
+               minc++;
+
+          if (arr[i] == maxi)
+               maxc++;
+     }
+
+     if (mini == maxi)
+     {
+          print(0LL);
+          print(minc * (minc - 1) / 2);
+     }
+     else
+     {
+          print(maxi - mini);
+          print(maxc * minc);
+     }
+}
