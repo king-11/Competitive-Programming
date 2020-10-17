243c731375fa61868b78e4b47294fcb0a4af35ec
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-10-17 11:57:16 +0530

    csoc number theory

--- csoc-number/O.cpp
+++ csoc-number/O.cpp
@@ -1,3 +1,6 @@
+#pragma comment(linker, "/stack:247474112")
+#pragma GCC optimize("Ofast")
+
 #include <bits/stdc++.h>
 
 #define rep(i, n) for (int i = 0; i < (n); ++i)
@@ -19,28 +22,58 @@
 #define vi vector<lld>
 #define si set<lld>
 #define endl "\n"
-#define int long long
+#define int unsigned long long
 #define pii pair<int, int>
 #define minq priority_queue<int, vector<int>, greater<int>>
 #define mem(a, val) memset(a, val, sizeof(a))
-#define X 1000000009
-#define le(x) scanf("%lld", &x)
-#define lle(x, y) scanf("%lld %lld", &x, &y)
+#define scani(x) scanf("%lld", &x)
+#define scani2(x, y) scanf("%lld %lld", &x, &y)
+#define scani3(x, y, z) scanf("%lld %lld %lld", &x, &y, &z);
 #define print(x) printf("%lld ", x)
 #define println(x) printf("%lld\n", x)
 #define SpeedForce ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000009
 
 using namespace std;
 
+const int N = 3e5 + 11;
+
 int32_t main()
 {
-#ifndef ONLINE_JUDGE
-     freopen("input.txt", "r", stdin);
-     freopen("output.txt", "w", stdout);
-#endif
+     // #ifndef ONLINE_JUDGE
+     //      freopen("input.txt", "r", stdin);
+     //      freopen("output.txt", "w", stdout);
+     // #endif
 
      SpeedForce;
+     int n, b;
+     scani2(n, b);
 
-     
-     
+     map<int, int> m1;
+     int temp = b;
+     for (int i = 2; i <= b; i++)
+     {
+          if (i * i > b)
+               i = b;
+          while (b % i == 0)
+          {
+               b /= i;
+               m1[i]++;
+          }
+     }
+
+     int ans = INT64_MAX;
+     tr(a, m1)
+     {
+          int num = 1, temp = 0;
+          while (num <= n / a.f)
+          {
+               num *= a.f;
+               temp += n / num;
+          }
+          temp /= a.s;
+          ans = min(temp, ans);
+     }
+
+     cout << ans;
 }
