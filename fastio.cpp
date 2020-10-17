daf09d394eeb73aaa136c49431ec89745f8f4503
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-10-17 11:56:16 +0530

    update template

--- fastio.cpp
+++ fastio.cpp
@@ -31,6 +31,47 @@
 #define SpeedForce ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define MOD 1000000009
 
+template <typename T>
+T gcd(T a, T b) { return (b ? __gcd(a, b) : a); }
+template <typename T>
+T lcm(T a, T b) { return (a * b) / gcd(a, b); }
+int mod_neg(int a, int b, int c)
+{
+     int res;
+     if (abs(a - b) < c)
+          res = a - b;
+     else
+          res = (a - b) % c;
+     return (res < 0 ? res + c : res);
+}
+template <typename T>
+T extended_euclid(T a, T b, T &x, T &y)
+{
+     T xx = 0, yy = 1;
+     y = 0;
+     x = 1;
+     while (b)
+     {
+          T q = a / b, t = b;
+          b = a % b;
+          a = t;
+          t = xx;
+          xx = x - q * xx;
+          x = t;
+          t = yy;
+          yy = y - q * yy;
+          y = t;
+     }
+     return a;
+}
+template <typename T>
+T mod_inverse(T a, T n)
+{
+     T x, y, z = 0;
+     T d = extended_euclid(a, n, x, y);
+     return (d > 1 ? -1 : mod_neg(x, z, n));
+}
+
 using namespace std;
 
 const int N = 3e5 + 11;
