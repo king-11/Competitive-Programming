68e52c5578b857c669fd146c62c90489c8304c6b
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-12-25 10:42:23 +0530

    690 dvi3  a b c

---
+++ Codeforces - Codeforces Round #690 (Div. 3)/B. Last Year's Substring/fastio.cpp
@@ -0,0 +1,89 @@
+/**
+ * @ Author: Lakshya Singh
+ * @ Create Time: 2020-10-14 14:56:31
+ * @ Modified by: Target_X
+ * @ Modified time: 2020-12-24 11:38:25
+ * @ Description:
+ */
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repA(i, a, n) for (int i = a; i <= (n); ++i)
+#define repD(i, a, n) for (int i = a; i >= (n); --i)
+#define tr(a, x) for (auto &a : x)
+#define all(c) c.begin(), c.end()
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define f first
+#define s second
+#define sz(a) int((a).size())
+#define present(c, x) (c.find(x) != c.end())
+#define cpresent(c, x) (find(all(c), x) != c.end())
+
+#define vi vector<int>
+#define si set<int>
+#define endl "\n"
+#define pii pair<int, int>
+#define minq priority_queue<int, vector<int>, greater<int>>
+#define mem(a, val) memset(a, val, sizeof(a))
+#define scani(x) scanf("%lld", &x)
+#define scani2(x, y) scanf("%lld %lld", &x, &y)
+#define scani3(x, y, z) scanf("%lld %lld %lld", &x, &y, &z);
+#define print(x) printf("%lld ", x)
+#define println(x) printf("%lld\n", x)
+#define SpeedForce ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define MOD 1000000009
+
+// available snippets
+// phi, seive, exponentiation, gcd, extended euclid, dfs, path lca, factorization
+// kmp search, segtree, dsu, ordered set, power2
+
+using namespace std;
+using namespace __gnu_pbds;
+
+const int N = 3e5 + 11;
+
+int32_t main()
+{
+     // #ifndef ONLINE_JUDGE
+     //      freopen("input.txt", "r", stdin);
+     //      freopen("output.txt", "w", stdout);
+     // #endif
+
+     SpeedForce;
+     int t;
+     cin >> t;
+     while (t--)
+     {
+          string s1;
+          int n;
+          cin >> n;
+          cin >> s1;
+
+          if (s1.substr(0, 4) == "2020" || s1.substr(n - 4, 4) == "2020")
+          {
+               cout << "YES" << endl;
+               continue;
+          }
+
+          if (s1[0] != '2' || s1[n - 1] != '0')
+          {
+               cout << "NO" << endl;
+               continue;
+          }
+
+          if ((s1[1] == '0' && s1[2] == '2') || (s1[1] == '0' && s1[n - 2] == '2') || (s1[n - 3] == '0' && s1[n - 2] == '2'))
+          {
+               cout << "YES" << endl;
+               continue;
+          }
+
+          cout << "NO" << endl;
+     }
+}
